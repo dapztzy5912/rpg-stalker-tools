197bbb2caab31eaa6b20d81440d6220bb3191315
dapztzy5912 <depannation999@gmail.com>
2025-06-07 17:02:22 +0700

    Create steam.py

---
+++ stalker/steam.py
@@ -0,0 +1,4 @@
+def stalk_steam():
+    user = input("Masukkan Steam ID/Nama pengguna: ")
+    print(f"🔍 Mengambil data Steam untuk {user} (simulasi)...")
+    print("🎮 Game: 154 | Jam Main: 2.345 Jam | Level: 37")
